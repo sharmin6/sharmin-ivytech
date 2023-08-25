3a41be1a42197c3fef3a06c4cf0270a4bd469de7
sharmin6 <143023594+sharmin6@users.noreply.github.com>
2023-08-25 16:38:55 -0400

    Add files via upload
    This file is for M01 Programming Assignment 4 including the UML diagram.

---
+++ M01PA4/RegularPolygon.java
@@ -0,0 +1,82 @@
+public class RegularPolygon {
+    private int n;
+    private double side;
+    private double x;
+    private double y;
+
+    public RegularPolygon() {
+        this.n = 3;
+        this.side = 1;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public RegularPolygon(int n, double side) {
+        this.n = n;
+        this.side = side;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public RegularPolygon(int n, double side, double x, double y) {
+        this.n = n;
+        this.side = side;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getPerimeter() {
+        return n * side;
+    }
+
+    public double getArea() {
+        return (n * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / n));
+    }
+
+    public static void main(String[] args) {
+        RegularPolygon polygon1 = new RegularPolygon();
+        RegularPolygon polygon2 = new RegularPolygon(6, 4);
+        RegularPolygon polygon3 = new RegularPolygon(10, 4, 5.6, 7.8);
+
+        System.out.println("Polygon 1 perimeter is " + polygon1.getPerimeter());
+        System.out.println("Polygon 1 area is " + polygon1.getArea());
+
+        System.out.println("Polygon 2 perimeter: " + polygon2.getPerimeter());
+        System.out.println("Polygon 2 area is " + polygon2.getArea());
+
+        System.out.println("Polygon 3 perimeter is " + polygon3.getPerimeter());
+        System.out.println("Polygon 3 area is " + polygon3.getArea());
+    }
+}
